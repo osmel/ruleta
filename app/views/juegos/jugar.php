c99bb01710f46c53e7ce4890f80debbcb68be9a9
lenovo-PC\Adrian <guerreroadrian1111@gmail.com>
2017-11-16 01:23:23 -0600

    cambios maquetas

--- app/views/juegos/jugar.php
+++ app/views/juegos/jugar.php
@@ -13,11 +13,55 @@
 
 <div class="intro juego">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center contadores">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+        <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+            <div class="wheelContainer">       
+                <svg class="wheelSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" text-rendering="optimizeSpeed">
+                    <defs>
+                        <filter id="shadow" x="-100%" y="-100%" width="550%" height="550%">
+                            <feOffset in="SourceAlpha" dx="0" dy="0" result="offsetOut"></feOffset>
+                            <feGaussianBlur stdDeviation="9" in="offsetOut" result="drop" />
+                            <feColorMatrix in="drop" result="color-out" type="matrix" values="0 0 0 0   0
+                      0 0 0 0   0 
+                      0 0 0 0   0 
+                      0 0 0 .3 0" />
+                            <feBlend in="SourceGraphic" in2="color-out" mode="normal" />
+                        </filter>
+                    </defs>
+                    <g class="mainContainer">
+
+                        <g class="wheel">
+                            <!-- <image  xlink:href="http://example.com/images/wheel_graphic.png" x="0%" y="0%" height="100%" width="100%"></image> -->
+                        </g>
+                    </g>
+                    <g class="centerCircle" />
+                    <g class="wheelOutline" />
+                    <g class="pegContainer" opacity="1">
+                        <path class="peg" fill="#EEEEEE" stroke="4" d="M22.139,0C5.623,0-1.523,15.572,0.269,27.037c3.392,21.707,21.87,42.232,21.87,42.232 s18.478-20.525,21.87-42.232C45.801,15.572,38.623,0,22.139,0z" />
+                    </g>
+                    <g class="valueContainer" />
+                </svg>
+
+                <img class="logo_centro" src="img/ley.png" border="0" style="width:150px; heigth:auto">
+                
+                <div class="toast">
+                    <p></p>                         
+                </div>
+
+                
+
+            </div>  
+        </div>
+
+
+
+
+
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
 			 <button type="button"    class="spinBtn btn btn-danger" >GIRAR</button>
 		</div>
 
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-center">
+		<!-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-center">
 			<div class="countdown">28.55</div>
 			<h5>SEGUNDOS RESTANTES</h5>
 
@@ -26,46 +70,10 @@
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-center">			
 			<div class="cant">2500</div>
 			<h5>CORAZONES ACUMULADOS <br>EN TU CUENTA</h5>
-		</div>		   	    	
+		</div>		 -->   	    	
 	</div>
 
-    <div class="wheelContainer">
-       
-        <svg class="wheelSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" text-rendering="optimizeSpeed">
-            <defs>
-                <filter id="shadow" x="-100%" y="-100%" width="550%" height="550%">
-                    <feOffset in="SourceAlpha" dx="0" dy="0" result="offsetOut"></feOffset>
-                    <feGaussianBlur stdDeviation="9" in="offsetOut" result="drop" />
-                    <feColorMatrix in="drop" result="color-out" type="matrix" values="0 0 0 0   0
-              0 0 0 0   0 
-              0 0 0 0   0 
-              0 0 0 .3 0" />
-                    <feBlend in="SourceGraphic" in2="color-out" mode="normal" />
-                </filter>
-            </defs>
-            <g class="mainContainer">
-
-                <g class="wheel">
-                    <!-- <image  xlink:href="http://example.com/images/wheel_graphic.png" x="0%" y="0%" height="100%" width="100%"></image> -->
-                </g>
-            </g>
-            <g class="centerCircle" />
-            <g class="wheelOutline" />
-            <g class="pegContainer" opacity="1">
-                <path class="peg" fill="#EEEEEE" stroke="4" d="M22.139,0C5.623,0-1.523,15.572,0.269,27.037c3.392,21.707,21.87,42.232,21.87,42.232 s18.478-20.525,21.87-42.232C45.801,15.572,38.623,0,22.139,0z" />
-            </g>
-            <g class="valueContainer" />
-        </svg>
-
-		<img class="logo_centro" src="img/ley.png" border="0" style="width:150px; heigth:auto">
-        
-        <div class="toast">
-         	<p></p>         	            
-        </div>
-
-        
-
-    </div>
+    
 
    
 
